adf87c0671894f5a3f6dd66ffa7c9d3fd9a84b61
ValentinBou <32929884+ValentinBou@users.noreply.github.com>
2018-03-22 10:39:01 +0100

    Update TypeBloc.java

--- core/src/main/java/imt/uvinfo/pacxon/modele/TypeBloc.java
+++ core/src/main/java/imt/uvinfo/pacxon/modele/TypeBloc.java
@@ -1,6 +1,39 @@
 package imt.uvinfo.pacxon.modele;
 
 public enum TypeBloc {
-	Bordure(0), Vide (1), Trace(2),TraceTouche(3), BlocNormal(4);
+	BORDURE(0), VIDE (1), TRACE(2),TRACETOUCHE(3), BLOCNORMAL(4);
+	
+	private int niveauBloc;
+	
+	public TypeBloc(int nb) {
+		niveauBloc = nb;
+	}
+	
+	public int getNiveauBloc() {
+		return niveauBloc;
+	}
+	
+	public String getTypeBloc() {
+		switch(this) {
+		case BORDURE : 
+			return "bordure";
+			//break;
+		case VIDE : 
+			return "vide";
+			//break;
+		case TRACE : 
+			return "trace";
+			//break;
+		case TRACETOUCHE : 
+			return "tracetouche";
+			//break;
+		case BLOCNORMAL : 
+			return "blocnormal";
+			//break;
+		default : 
+			return "erreur";
+			//break;
+		}
+	}
 	
 }
